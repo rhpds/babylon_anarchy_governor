852a50173517ab1aaf7a5fb8a2efa43582781bd0
Johnathan Kupferer <jkupfere@redhat.com>
2020-08-10 15:34:28 -0500

    Add save dynamic_job_vars

---
+++ filter_plugins/babylon.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2020 Red Hat
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import AnsibleFilterError
+from ansible.module_utils.six.moves.urllib.parse import urlsplit
+from ansible.utils import helpers
+
+from copy import deepcopy
+from datetime import datetime, timedelta
+
+import re
+
+def filter_job_vars_to_dynamic_job_vars(anarchy_governor, preserve_job_vars):
+    return {
+        k: v for (k, v) in anarchy_governor['spec']['vars'].get('job_vars', {}).items() if k in preserve_job_vars
+    }
+
+def filter_job_vars_secrets_to_dynamic_job_vars(anarchy_governor):
+    dynamic_job_var_secrets = []
+    for var_secret in anarchy_governor['spec'].get('varSecrets', []):
+        if var_secret.get('var', '') == 'job_vars':
+            dynamic_job_var_secret = deepcopy(var_secret)
+            dynamic_job_var_secret['var'] = 'dynamic_job_vars'
+            dynamic_job_var_secrets.append(dynamic_job_var_secret)
+    return dynamic_job_var_secrets
+
+# ---- Ansible filters ----
+class FilterModule(object):
+    ''' URI filter '''
+
+    def filters(self):
+        return {
+            'filter_job_vars_to_dynamic_job_vars': filter_job_vars_to_dynamic_job_vars,
+            'filter_job_vars_secrets_to_dynamic_job_vars': filter_job_vars_secrets_to_dynamic_job_vars,
+        }
