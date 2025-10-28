4651959e459b5ee847252f480db5a9137cfd3c02
Johnathan Kupferer <jkupfere@redhat.com>
2025-10-28 14:47:38 -0400

    Fix templated_var lookup (#124)

--- lookup_plugins/templated_var.py
+++ lookup_plugins/templated_var.py
@@ -16,13 +16,28 @@ options:
     required: True
 """
 
+from collections.abc import Iterable
+
 from ansible.plugins.lookup import LookupBase
+from ansible.template import trust_as_template
+
+def recursive_trust_as_template(arg):
+    if isinstance(arg, str):
+        return trust_as_template(arg)
+    elif isinstance(arg, dict):
+        return {
+            key: recursive_trust_as_template(value) for key, value in arg.items()
+        }
+    elif isinstance(arg, Iterable):
+        return [ recursive_trust_as_template(value) for value in arg ]
+    else:
+        return arg
 
 class LookupModule(LookupBase):
     def run(self, terms, variables, **kwargs):
         ret = []
         for term in terms:
-            variables['__term__'] = term
-            out = self._templar.template('{{ __term__ }}')
+            variables['__term__'] = recursive_trust_as_template(term)
+            out = self._templar.template(trust_as_template('{{ __term__ }}'))
             ret.append(out)
         return ret
