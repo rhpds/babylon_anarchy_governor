37a40e1fa78f1fb3d9d70c1af46ceb03bb5ec597
Guillaume Coré <gucore@redhat.com>
2026-03-22 18:38:39 +0100

    feat: add queue handling for sandbox placements
    Add support for handling queued placements from sandbox API.
    
    - Introduce `sandbox_api_queue_check_interval` variable to control how often
      queued placements are re-checked
    - Add new state `provision-queued` to track placements waiting in queue
    - Implement periodic checks for queued placements via `anarchy_continue_action`
    - Update placement status checks to recognize 'queued' state
    - Add test plugin to capture continue action calls for verification

---
+++ tests/action_plugins/anarchy_continue_action.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import json
+import os
+from copy import deepcopy
+from ansible.plugins.action import ActionBase
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None, **_):
+        result = super(ActionModule, self).run(tmp, task_vars)
+        test_output_dir = task_vars.get('test_output_dir', '.')
+        fh = open(os.path.join(test_output_dir, 'anarchy_continue_actions.yaml'), 'a')
+        fh.write('- ' + json.dumps(self._task.args) + '\n')
+        result.update(deepcopy(self._task.args))
+        return result
